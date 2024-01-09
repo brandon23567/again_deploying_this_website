a27065126ef28b60dd1f8b458d50ce492e776c93
brandon23567 <brandonmahuvava0@gmail.com>
2024-01-10 00:59:50 +0530

    deployin this to see whats actually wrong here

---
+++ webapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def index(request):
+    return Response({"message": "Hello there using another mans project"})
